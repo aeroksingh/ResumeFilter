b73baf5d8cef0c58443d96b9dd9c4361ba634829
aeroksingh <assign131@gmail.com>
2026-03-03 14:15:39 +0530

    .gitignore added

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const userModel = require("../models/user.model")
+
+
+/**
+ * @route POST /api/auth/Register
+ * @description Register a mew user,expects username,email
+ * @access Public
+ */
+
+async function registerUserController(req,res) {}
+
+module.exports = {}
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+const {Router} = require('express')
+const app = require('../app')
+const authController = require("../controllers/auth.controller")
+const authRouter = Router()
+
+/**
+ * @route POST /api/auth/Register
+ * @description Register a mew user
+ * @access Public
+ */
+
+
+module.exports = {}
